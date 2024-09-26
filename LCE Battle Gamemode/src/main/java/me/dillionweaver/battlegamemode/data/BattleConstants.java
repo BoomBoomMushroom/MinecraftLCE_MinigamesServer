0b03be54d344300392921e68efe5d7fb2f32477c
BoomBoomMushroom <dillioncat7@gmail.com>
2024-09-26 11:58:21 -0400

    words

--- LCE Battle Gamemode/src/main/java/me/dillionweaver/battlegamemode/data/BattleConstants.java
+++ LCE Battle Gamemode/src/main/java/me/dillionweaver/battlegamemode/data/BattleConstants.java
@@ -5,9 +5,9 @@ import org.bukkit.util.Vector;
 
 public class BattleConstants {
 
-    public static final String[] idToName = new String[] { "Lobby", "Crucible (8 Players)" };
+    public static final String[] idToName = new String[] { "Lobby", "Crucible (8 Players)", "Crucible (16 Players)" };
     public static final int[] votableMapIds = new int[]{
-            1,
+            1, 2, // Both Crucibles
     };
     public static final int[][] allSpawnPoints = new int[][] {
             new int[] { -340, 58, -341 }, // Lobby              0
@@ -29,18 +29,27 @@ public class BattleConstants {
             new int[] { -774, 41, 129 }, // crucible (8 plr)    15
             new int[] { -780, 41, 127 }, // crucible (8 plr)
 
-            new int[] { -897, 41, -341 }, // crucible (16 plr)
-            new int[] { -0, 41, -0 }, // crucible (16 plr)
-            new int[] { -0, 41, -0 }, // crucible (16 plr)
-            new int[] { -0, 41, -0 }, // crucible (16 plr)
-            new int[] { -0, 41, -0 }, // crucible (16 plr)
-            new int[] { -0, 41, -0 }, // crucible (16 plr)
-            new int[] { -0, 41, -0 }, // crucible (16 plr)
-            new int[] { -0, 41, -0 }, // crucible (16 plr)
+            new int[] { -897, 48, -341 }, // crucible (16 plr)
+            new int[] { -904, 48, -337 }, // crucible (16 plr)
+            new int[] { -911, 48, -341 }, // crucible (16 plr)
+            new int[] { -915, 48, -348 }, // crucible (16 plr)  20
+            new int[] { -911, 48, -355 }, // crucible (16 plr)
+            new int[] { -904, 48, -359 }, // crucible (16 plr)
+            new int[] { -897, 48, -355 }, // crucible (16 plr)
+            new int[] { -893, 48, -348 }, // crucible (16 plr)
+            new int[] { -894, 45, -328 }, // crucible (16 plr)  25
+            new int[] { -924, 45, -340 }, // crucible (16 plr)
+            new int[] { -925, 38, -359 }, // crucible (16 plr)
+            new int[] { -914, 45, -368 }, // crucible (16 plr)
+            new int[] { -830, 33, -362 }, // crucible (16 plr)
+            new int[] { -883, 45, -358 }, // crucible (16 plr)  30
+            new int[] { -884, 39, -337 }, // crucible (16 plr)
+            new int[] { -918, 33, -334 }, // crucible (16 plr)
     };
     public static final int[][] mapIdToSpawnPointIndexes = new int[][] {
             new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 }, // lobby
             new int[] { 9, 10, 11, 12, 13, 14, 15, 16 }, // crucible 8 plr
+            new int[] { 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32 }, // crucible 16 plr
     };
 
     public static final Material[] cannotDropItems = new Material[]{
